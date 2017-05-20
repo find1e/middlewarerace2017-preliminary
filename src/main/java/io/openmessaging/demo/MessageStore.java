2253eb6f94d8c8af989ea556a8914c728d1e82ae
fbhw <jyunchyou@gmail.com>
2017-05-20 12:04:10 +0800

    ,,

--- src/main/java/io/openmessaging/demo/MessageStore.java
+++ src/main/java/io/openmessaging/demo/MessageStore.java
@@ -48,7 +48,7 @@ public class MessageStore  {
 
     private HashMap<String,FileChannelProxy> topicMap = new HashMap(120);
 
-    private DistributeLock sendLock=new DistributeLock(140);
+    private DistributeLock sendLock=new DistributeLock(160);
 
     final ThreadLocal threadLocal=new ThreadLocal();
 
